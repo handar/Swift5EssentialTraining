d51ae0a0d755f693a60fb33230542acbd025b38f
handar <hadiaandar@gmail.com>
2021-06-02 16:59:14 -0700

    dictionary methods

--- Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Core Dictionary Methods.xcplaygroundpage/Contents.swift
+++ Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Core Dictionary Methods.xcplaygroundpage/Contents.swift
@@ -16,10 +16,29 @@ Dictionary elements can be easily updated and removed using subscript syntax or 
 
  */
 // Caching and overwriting items
+var playerStats: [String:Int] = ["HP": 100, "Attack": 35, "Gold": 29]
+var oldValue = playerStats.updateValue(30, forKey: "Attack") //will create a keyvalue pair if it doesnt exist
 
+//playerStats = ["Evasion": 12, "MP": 55] //gonna override dictionary elements with the new one
 
 // Caching and removing items
+//playerStats["Gold"] = nil //removes gold from the dictionary
+var removedValue = playerStats.removeValue(forKey: "Gold")
 
+print(playerStats)
 
 // Nested dictionaries
+var questBoard = [
+    //value for each key is going to be a dictionary
+    "Fetch Gemstones": [
+        "Objective": "Retrieve 5 gemstones",
+        "Secret": "Complete in under 5 minutes"
+    ],
+    "Defeat Big Boss": [
+        "Objective": "Beat the boss",
+        "Secret": "Win with 50% health"
+    ]
+]
 
+//access Fetch Gemstone quest
+var gemstoneObjective = questBoard["Fetch Gemstones"]?["Objective"] //? means break the return chain if any of the keys dont exist and return a nil value
